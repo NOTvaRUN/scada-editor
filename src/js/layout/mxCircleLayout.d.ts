f9647bbb4483fb291b2384810a8b6b40c6a70ab4
Varun Nayak <varunnayak652@gmail.com>
2021-12-16 15:27:40 +0530

    Type definitions added for angular updates

---
+++ src/js/layout/mxCircleLayout.d.ts
@@ -0,0 +1,14 @@
+declare class mxCircleLayout {
+  x0: number;
+  y0: number;
+  radius: number;
+  moveCircle: boolean;
+  disableEdgeStyle: boolean;
+
+  constructor(graph: mxGraph, radius?: number);
+
+  execute(parent: mxCell): void;
+
+  getRadius(count: number, max: number): number;
+
+}
